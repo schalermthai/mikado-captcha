b383b3e41496ae793433f38fed4db8307b7fa382
schalerm <schalerm@thoughtworks.com>
2019-08-31 02:20:25 +0700

    make ValueNode implements Node

--- src/main/java/com/thoughtworks/xconf/th/captcha/ValueNode.java
+++ src/main/java/com/thoughtworks/xconf/th/captcha/ValueNode.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.xconf.th.captcha;
 
-public class ValueNode {
+public class ValueNode implements Node {
     protected Integer value = 0;
+
+    @Override
+    public Integer compute() {
+        return value;
+    }
+
+    @Override
+    public String display() {
+        return value + "";
+    }
 }
